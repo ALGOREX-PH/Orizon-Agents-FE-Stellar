09364b2f473a3df2a3754c6d59970e9a81fd3444
Danielle Bagaforo Meer <algorexph@gmail.com>
2026-05-14 00:22:13 +0800

    added __init__ module

---
+++ backend/app/demo_kits/__init__.py
@@ -0,0 +1,13 @@
+from .registry import ALL_KITS, detect_kit, kit_by_id
+from .schemas import BrandSpec, DemoKit, FeatureSpec, PaletteSpec, TypographySpec
+
+__all__ = [
+    "ALL_KITS",
+    "detect_kit",
+    "kit_by_id",
+    "DemoKit",
+    "BrandSpec",
+    "FeatureSpec",
+    "PaletteSpec",
+    "TypographySpec",
+]
